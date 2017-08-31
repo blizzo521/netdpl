d2fc456ef02dacba7d78983984d6df1a2d60cd31
Morgan Whitney <morganwhitney@gmail.com>
2017-08-30 22:29:10 -0600

    refactor mechanize stuff into separate model

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -12,45 +12,16 @@ class DashboardController < ApplicationController
     loans_page = nil
     loans = []
 
-    name = ''
-    a = Mechanize.new { |agent|
-      agent.follow_meta_refresh = true
-      agent.user_agent_alias = 'Mac Firefox'
-    }
-
-    a.get('https://catalog.denverlibrary.org/logon.aspx') do |signin|
-      signin_page = signin
-
-      form = signin_page.form_with(:action => './logon.aspx')
-      form.textboxBarcodeUsername  = @current_user.dpl_username
-      form.textboxPassword = @current_user.dpl_password
-
-      my_page = form.submit(form.buttons.first)
-
-      name = my_page.search('#ctrlBasicInfo_labelPatron').text
-      name = name.split(', ').reverse.join(' ').split(' ').map(&:capitalize).join(' ')
-
-      loans_page = my_page.links.find { |l| l.text.match(/items out/i)}.click
-
-      loan_rows = loans_page.search('.patrongrid-row, .patrongrid-alternating-row')
-      loan_rows.each do |loan|
-        type = loan.search('td:nth-child(3) img').first['alt']
-        title = loan.search('td:nth-child(5)').text.strip
-        due = loan.search('td:nth-child(7)').text.strip
-        renewals = loan.search('td:nth-child(8)').text.strip
-
-        loans << {type: type, title: title, due: due, renewals: renewals}
-      end
-    end
+    dpl = DPL.new(@current_user)
 
     @netdplapp_props = {
       user: {
         email: @current_user.email,
-        name: name,
+        name: dpl.name,
       },
-      books: [{id: 5}],
-      loans: loans,
-      holds: [{id: 1}],
+      books: dpl.books,
+      loans: dpl.loans,
+      holds: dpl.holds,
     }
   end
 
--- app/models/dpl.rb
+++ app/models/dpl.rb
@@ -1,7 +1,51 @@
+require 'rubygems'
+require 'mechanize'
+require 'pry-remote'
+
 class DPL
-  class << self
-    def test
-      'test yoself!'
+  attr_reader :holds, :books, :name, :loans
+
+  def initialize(user)
+    # Instance variables
+    @user = user
+    @name = ''
+    @loans = []
+    @books = []
+    @holds = []
+    @agent = Mechanize.new { |agent|
+      agent.follow_meta_refresh = true
+      agent.user_agent_alias = 'Mac Firefox'
+    }
+
+    load_initial_data
+  end
+
+  private
+
+  def load_initial_data
+    @agent.get('https://catalog.denverlibrary.org/logon.aspx') do |signin|
+      signin_page = signin
+
+      form = signin_page.form_with(:action => './logon.aspx')
+      form.textboxBarcodeUsername  = @user.dpl_username
+      form.textboxPassword = @user.dpl_password
+
+      my_page = form.submit(form.buttons.first)
+
+      @name = my_page.search('#ctrlBasicInfo_labelPatron').text
+      @name = @name.split(', ').reverse.join(' ').split(' ').map(&:capitalize).join(' ')
+
+      loans_page = my_page.links.find { |l| l.text.match(/items out/i)}.click
+
+      loan_rows = loans_page.search('.patrongrid-row, .patrongrid-alternating-row')
+      loan_rows.each do |loan|
+        type = loan.search('td:nth-child(3) img').first['alt']
+        title = loan.search('td:nth-child(5)').text.strip
+        due = loan.search('td:nth-child(7)').text.strip
+        renewals = loan.search('td:nth-child(8)').text.strip
+
+        @loans << {type: type, title: title, due: due, renewals: renewals}
+      end
     end
   end
 end
